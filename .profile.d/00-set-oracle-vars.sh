efa4abf787c1c8b1fdafc86bb542843235300e6c
Troy Martin <troy@devatotech.com>
2014-11-26 02:32:56 -0500

    updated export

--- .profile.d/00-set-oracle-vars.sh
+++ .profile.d/00-set-oracle-vars.sh
@@ -1,3 +1,3 @@
 # Set default buildpack path.
-export ORACLE_HOME="${LD_LIBRARY_PATH}:/vendor"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/vendor"
+export ORACLE_HOME=vendor/oracle_instantclient
+export LD_LIBRARY_PATH=vendor/oracle_instantclient
